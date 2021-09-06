35a4f2af9018c62175f536edecd3c99125a8c17c
Anton Zhdanok <kvansipto@gmail.com>
2021-09-06 23:54:23 +0700

    add TestUserAddToBasketFromProductPage and ready for review

--- pages/locators.py
+++ pages/locators.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_invalid")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a")
     BASKET_ITEMS = (By.CLASS_NAME, ".basket-items")
     EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
-    # EMPTY_BASKET_MESSAGE = (By.XPATH, "//p[contains(text(), 'Ваша корзина пуста')]")
 
 
 class MainPageLocators:
@@ -20,6 +20,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_USER_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_USER_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTER_USER_CONFIRM_PASSWORD = (By.ID, "id_registration-password2")
+    REGISTER_SUBMIT_BUTTON = (By.NAME, "registration_submit")
 
 
 class ProductPageLocators:
--- pages/login_page.py
+++ pages/login_page.py
@@ -16,3 +16,10 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_USER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_USER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_USER_CONFIRM_PASSWORD).send_keys(password)
+        submit_button = self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT_BUTTON)
+        submit_button.click()
